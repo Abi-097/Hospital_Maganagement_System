1c6626702d75c6faee218797d3eba87ac8b03afc
Abi-097 <abimahen0312@gmail.com>
2023-08-05 10:01:37 +0530

    ui creating

---
+++ Pages/Main/script.js
@@ -0,0 +1,4 @@
+function sayHello() {
+    // alert('Hello, World!');
+  }
+  document.addEventListener('DOMContentLoaded', sayHello);
---
+++ Pages/Patients/script.js
@@ -0,0 +1,24 @@
+// const backButton = document.getElementById("backButton");
+
+//     // Add a click event listener
+//     backButton.addEventListener("click", function() {
+//         // Redirect to the "bed.html" page when the button is clicked
+//         window.location.href = "bed.html";
+//     });
+
+
+    // const backButton = document.getElementById("backButton")
+    // backButton.addEventListener("click", function() {
+    //     window.location.href = "Main.html"
+    // } )
+
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     // Select the button by its ID
+    //     const backButton = document.getElementById("backButton");
+    
+    //     // Add a click event listener
+    //     backButton.addEventListener("click", function() {
+    //         // Redirect to the "bed.html" page when the button is clicked
+    //         window.location.href = "Main.html";
+    //     });
+    // });
